d094d957a2e07efe958bff58d1df23c2ca44ec21
Jason Zubiate <jzubiate.dev@gmail.com>
2025-04-11 12:24:53 -0700

    adds matterjs

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
 
   return (
     <section id="footer" className="p-4">
-      <footer className="flex flex-col justify-between gap-8 p-4 md:px-8 pt-8 pb-6 rounded-xl bg-stone-100 h-[600px] lg:h-[700px]">
+      <footer className="flex flex-col justify-between gap-8 p-4 md:px-8 pt-8 pb-6 rounded-xl bg-stone-100 h-[600px] lg:h-[800px] xl:h-[900px]">
         <div className="flex justify-between w-full">
           <p className="font-semibold text-[clamp(16px,1.6vw,24px)]">
             Los Angeles, CA
@@ -22,7 +22,7 @@ export default function Footer() {
 
         <div
           ref={matterContainer}
-          className="h-full relative overflow-hidden border-2 border-red-500"
+          className="h-full relative overflow-hidden"
         >
           <h2 className="text-[clamp(50px,6vw,72px)] font-semibold text-center tracking-tight leading-[0.85] top-12 lg:top-6 left-1/2 -translate-x-1/2 absolute w-full">
             Let&apos;s work{" "}
