d36745ebbde74bb3454ba37391c5b5e683068b11
Jason Zubiate <jzubiate.dev@gmail.com>
2025-04-15 19:13:47 -0700

    Add video files for social links and update video URLs in socialLinks.ts

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -8,25 +8,21 @@ export const socialLinks: SocialLink[] = [
   {
     title: "Instagram",
     url: "https://www.instagram.com/jazziwong_/",
-    videoUrl:
-      "https://www.dropbox.com/scl/fi/b8fwmhhpqrchg3e6vmty5/instagram.mov?rlkey=30wml8gk4k3ujfv49tv0iav5f&st=s6jpwbkd&dl=1",
+    videoUrl: "videos/instagram.webm",
   },
   {
     title: "YouTube",
     url: "https://www.youtube.com/@jazziwong",
-    videoUrl:
-      "https://www.dropbox.com/scl/fi/lp3ehvikb8f72gtqze8ge/youtube.mov?rlkey=2zkaiyteq9vc6e9g5g7lheozw&st=j94h7dee&dl=1",
+    videoUrl: "videos/youtube.webm",
   },
   {
     title: "Unsplash",
     url: "https://unsplash.com/@jazziwong",
-    videoUrl:
-      "https://www.dropbox.com/scl/fi/yhvzc3b2fixxbmtn9pyuw/unsplash.mov?rlkey=ku4ppy3kosq1eiyye7vweeett&st=ca09z286&dl=1",
+    videoUrl: "videos/unsplash.webm",
   },
   {
     title: "TikTok",
     url: "https://www.tiktok.com/@jazziwong",
-    videoUrl:
-      "https://www.dropbox.com/scl/fi/41vwpoips67ajemdbuqh0/tiktok.mov?rlkey=lu8v6q5tw7rvcubz0l6u4zbdg&st=naxn2dka&dl=1",
+    videoUrl: "videos/tiktok.webm",
   },
 ];
